a03b84f78ac5b263c141d68b804d4feb904131c6
Andrey Kuznetsov <andreisk2005@gmail.com>
2025-03-17 09:21:39 +0300

    Начал делать страницу просмотра специалитета конкретного специалиста (врача).

---
+++ HealthPatient/ViewModels/CheckSpecialtyViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HealthPatient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthPatient.ViewModels
+{
+    public partial class CheckSpecialtyViewModel:ViewModelBase
+    {
+        [ObservableProperty] Doctor doctor;
+
+        public CheckSpecialtyViewModel()
+        {
+            doctor = MainWindowViewModel.Instance.Doctor;
+        }
+    }
+}
---
+++ HealthPatient/Views/CheckSpecialtyView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HealthPatient.Views;
+
+public partial class CheckSpecialtyView : UserControl
+{
+    public CheckSpecialtyView()
+    {
+        InitializeComponent();
+    }
+}
